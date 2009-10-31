6ff8a39548ba1e762d679548564376b0b0b54c45
ejonatan.castro <ejonatan.castro@de7e9696-c0cf-11de-bbac-836dcc338a7a>
2009-10-31 02:35:23 +0000

    Con paquetes, DAO y algunas cosas mas

--- source/Arquero.java
+++
@@ -1,2 +0,0 @@
-public class Arquero extends Jugador {
-}
--- source/Observable.java
+++
@@ -1,8 +0,0 @@
-public interface Observable {
-
-	public void addObserver(Object aObserver_o);
-
-	public void removeObserver(Object aObserver_o);
-
-	public void notifyObservers();
-}
