233d2727aa19be00643aed1eb1bdf31f677c2408
cande89 <cande89@de7e9696-c0cf-11de-bbac-836dcc338a7a>
2009-11-10 23:50:15 +0000

    Clase Usuario

--- trunk/source/logica/Usuario.java
+++ trunk/source/logica/Usuario.java
@@ -28,17 +28,111 @@ public class Usuario {
 	private float montoDisponible;
 	private float montoGastado;
 	private Vector<Torneo> torneos;
-
-
-	public void update() {
-		throw new UnsupportedOperationException();
+	public String getNombre() {
+		return nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public String getTipoDoc() {
+		return tipoDoc;
+	}
+	public int getNroDoc() {
+		return nroDoc;
+	}
+	public Date getFechaNac() {
+		return fechaNac;
+	}
+	public String getSexo() {
+		return sexo;
+	}
+	public String getHincha() {
+		return hincha;
+	}
+	public String getProvincia() {
+		return provincia;
+	}
+	public String getPartido() {
+		return partido;
+	}
+	public String getLocalidad() {
+		return localidad;
+	}
+	public String getCalle() {
+		return calle;
+	}
+	public int getNumero() {
+		return numero;
+	}
+	public int getPiso() {
+		return piso;
+	}
+	public String getDpto() {
+		return dpto;
+	}
+	public String getCp() {
+		return cp;
+	}
+	public int getTel() {
+		return tel;
+	}
+	public int getCel() {
+		return cel;
+	}
+	public String getProveedorCel() {
+		return proveedorCel;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public Equipo getEquipo() {
+		return equipo;
+	}
+	public float getMontoDisponible() {
+		return montoDisponible;
+	}
+	public float getMontoGastado() {
+		return montoGastado;
+	}
+	public Vector<Torneo> getTorneos() {
+		return torneos;
+	}
+	
+	// constructor
+	public Usuario(String nombre, String apellido, String tipoDoc, int nroDoc,
+			Date fechaNac, String sexo, String hincha, String provincia,
+			String partido, String localidad, String calle, int numero,
+			int piso, String dpto, String cp, int tel, int cel,
+			String proveedorCel, String email, String password) {
+		super();
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.tipoDoc = tipoDoc;
+		this.nroDoc = nroDoc;
+		this.fechaNac = fechaNac;
+		this.sexo = sexo;
+		this.hincha = hincha;
+		this.provincia = provincia;
+		this.partido = partido;
+		this.localidad = localidad;
+		this.calle = calle;
+		this.numero = numero;
+		this.piso = piso;
+		this.dpto = dpto;
+		this.cp = cp;
+		this.tel = tel;
+		this.cel = cel;
+		this.proveedorCel = proveedorCel;
+		this.email = email;
+		this.password = password;
 	}
 
-	public void getDNI() {
-		throw new UnsupportedOperationException();
-	}
+	
 
-	public void operation() {
-		throw new UnsupportedOperationException();
 	}
-}
