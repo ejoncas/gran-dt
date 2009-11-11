18483d56ac39bac74e0de35a63a4413902afb49c
ejonatan.castro <ejonatan.castro@de7e9696-c0cf-11de-bbac-836dcc338a7a>
2009-11-11 02:43:18 +0000

    casi toda el alta del usuario linkeada con el controlller y funcionando, falta la etapa 3 ya que no habias hecho las validaciones y es muy tarde como para ponerme a hacerlas. Mañana lo termino!

--- source/logica/SistemaGranDT.java
+++ source/logica/SistemaGranDT.java
@@ -32,7 +32,7 @@ public class SistemaGranDT {
 		this.torneos=new Vector<Torneo>();
 	}
 
-	public SistemaGranDT getInstance(){
+	public static SistemaGranDT getInstance(){
 		//This method return the unique instance - SINGLETON
 		return grandt==null ? new SistemaGranDT() : grandt;		
 	}
@@ -49,17 +49,16 @@ public class SistemaGranDT {
 	public Vector getTorneos() {
 		return torneos;
 	}
-	
-	
-	
+
+
+
 	/********** validaciones *************/
 	// valida el DNI: si ya existe un usuario con el dni devuelve true, si no false.
 	public boolean validarDNI (String tipo, int nro){
-		Vector usuarios = this.getUsuarios();
 		String t;
 		int n;
 		for(int i=0; i<usuarios.size();i++){
-			Usuario u = (Usuario) usuarios.elementAt(i);
+			Usuario u = usuarios.elementAt(i);
 			n = u.getNroDoc();
 			t = u.getTipoDoc();
 			if (tipo.equals(t) && nro == n)
@@ -67,21 +66,21 @@ public class SistemaGranDT {
 		}			
 		return false;		
 	}
-	
+
 	// crea un usuario y lo agrega al vector de usuarios
 	public void crearUsuario(String nombre, String apellido, String tipoDoc, int nroDoc,
 			Date fechaNac, String sexo, String hincha, String provincia,
 			String partido, String localidad, String calle, int numero,
 			int piso, String dpto, String cp, int tel, int cel,
 			String proveedorCel, String email, String password){
-		
+
 		Usuario u = new Usuario(nombre,  apellido,  tipoDoc,  nroDoc,
-			 fechaNac,  sexo,  hincha, provincia,
-			 partido,  localidad,  calle, numero,
-			 piso,  dpto,  cp,  tel,  cel,
-			 proveedorCel,  email,  password);
-		
+				fechaNac,  sexo,  hincha, provincia,
+				partido,  localidad,  calle, numero,
+				piso,  dpto,  cp,  tel,  cel,
+				proveedorCel,  email,  password);
+
 		this.usuarios.addElement(u);
-		
+
 	}
 }
