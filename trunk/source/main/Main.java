6b0497a76d3ebf323567c900a9103466978b829b
ejonatan.castro <ejonatan.castro@de7e9696-c0cf-11de-bbac-836dcc338a7a>
2009-11-11 02:44:24 +0000

    Me olvide de agregar el main nuevo y un frame

---
+++ trunk/source/main/Main.java
@@ -0,0 +1,38 @@
+
+package main;
+
+
+import logica.Jugador;
+import logica.dao.JugadorDAO;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Jugador j = new Jugador("Jonatan", "Castro", "Almagro",2000,10,12,10000);
+
+		System.out.println(j.getEdad());
+
+		JugadorDAO jdao = new JugadorDAO();
+		Jugador loaded = jdao.getJugadorPorId(1704);
+
+		//Vector<Jugador> jugadoresVolantes = jdao.getJugadoresPorPosicion("VOL");
+
+		//jdao.insertJugador(j);
+
+
+		//for(Jugador jug : jugadoresVolantes){
+		//		System.out.println(jug.toString());
+		//}
+
+		System.out.println(loaded.toString());
+
+		//new AltaUsuario1();
+
+
+	}
+
+}
